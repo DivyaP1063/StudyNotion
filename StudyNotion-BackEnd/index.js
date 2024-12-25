097428ff76141aaeb72b34d923a27282393a5d39
Divya Prakash <119873792+DivyaP1063@users.noreply.github.com>
2024-12-26 03:24:32 +0530

    Update index.js

--- StudyNotion-BackEnd/index.js
+++ StudyNotion-BackEnd/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://studynotion-g2qt.onrender.com",
 		credentials:true,
 	})
 )
